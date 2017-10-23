2b7974345f2248a0cb6c73d3fdaf2c561c161c87
westnorthern <rrichardson610@gmail.com>
2017-10-23 11:29:59 -0600

    created basic input (letter guess) method

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 
-const phraseArray = ['quixotic'];
+const phraseArray = ['for tune'];
 
 
 class Game {
@@ -9,7 +9,8 @@ class Game {
 
     this.phraseArr = phraseArr;
     this.phrase = this.phraseArr[0].split('');
-    this.correctLetters = [];
+    this.correctLetters = ['n', 't'];
+    this.incorrectLetters = [];
     this.abcArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
     this.phraseLetters =  this.phrase.filter(function(elem, index, self) 
     {
@@ -22,17 +23,28 @@ class Game {
     if (this.correctLetters.length == this.phraseLetters.length) {
       alert('You win!');
     }
-    else {
-      this.guessLetter();
+    else{
+      this.displayPhrase();
     }
   } // End of checkIfWon method
 
-  displayClue(){
+  displayPhrase(){
+    $('#phrase-display').empty();
 
+    for (var i = 0; i < this.phrase.length; i++) {
+      if (this.phrase[i] === ' '){
+        $('#phrase-display').append('<div class="col-1 phrase-letter" style="background: black;"></div>');
+      }
+      else if (this.correctLetters.includes(this.phrase[i])){
+        $('#phrase-display').append(`<div class="col-1 phrase-letter">${this.phrase[i]}</div>`);
+      }
+      else {
+        $('#phrase-display').append(`<div class="col-1 phrase-letter"></div>`);
+      }
+    }
   }
 
-  guessLetter(){
-    let letter = prompt('Guess a letter!');
+  guessLetter(letter){
 
 
     if(this.phrase.includes(letter)){
@@ -45,8 +57,18 @@ class Game {
       }
     }
 
+    if (this.phrase.includes(letter) == false) {
+      this.incorrectLetters.push(letter);
+    }
+
     this.checkIfWon();
 
+  }
+
+  displayLetters() {
+    for (var i = 0; i < this.abcArr.length; i++) {
+      $('#letters-list').append(`<div class="col-1 abcs" data-letter="${this.abcArr[i]}"> ${this.abcArr[i].toUpperCase()}</div>`);
+    }
   }
 
 
@@ -63,10 +85,20 @@ class Game {
 
 $(function() { // Document ready function
   
+
+  $(document).on('click', '.abcs', function(){
+    let letter = $(this).data('letter');
+    wof.guessLetter(letter);
+  })
+
   let wof = new Game(phraseArray);
+
+
 
   // wof.displayClue();
   // wof.checkIfWon();
+  wof.displayPhrase();
+  wof.displayLetters();
 
 
 }); // End of Document Ready Function
